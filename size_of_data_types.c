d650b91033df749599e92f0db00c02b64cc13f8c
Suraj Kumar Saw <suraj03saw@gmail.com>
2024-06-01 12:17:13 +0530

    Add files via upload

---
+++ size_of_data_types.c
@@ -0,0 +1,13 @@
+// program to print size of various data types in C programming language.
+
+#include<stdio.h>
+void main()
+{
+	printf("size of int: %zu bytes\n", sizeof(int));
+	printf("size of float: %zu bytes\n", sizeof(float));
+	printf("size of double: %zu bytes\n", sizeof(double));
+	printf("size of short: %zu bytes\n", sizeof(short));
+	printf("size of char: %zu bytes\n", sizeof(char));
+	printf("size of long: %zu bytes\n", sizeof(long));
+}
+
